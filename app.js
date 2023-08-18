48a777f59d3c1d15828ddfb3e8f029b5d4117f11
Aris Nguyen <nda1291@outlook.com>
2023-08-18 10:46:02 +0700

    get base url from env

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var pokemonRouter = require("./routes/pokemon-api");
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -18,37 +19,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-  cors({
-    // origin: "http://localhost:3000",
-    origin: "https://main--pokemon-render-0815.netlify.app",
-  })
+	cors({
+		// origin: process.env.BASE_URL,
+		origin: "https://main--pokemon-render-0815.netlify.app",
+	})
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Headers", "Authorization");
-  res.setHeader("Access-Control-Allow-Headers", "Origin");
-  res.setHeader("Access-Control-Allow-Headers", "X-Auth-Token");
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	res.setHeader("Access-Control-Allow-Headers", "Authorization");
+	res.setHeader("Access-Control-Allow-Headers", "Origin");
+	res.setHeader("Access-Control-Allow-Headers", "X-Auth-Token");
+	next();
 });
 
 app.use("/", indexRouter);
+app.use("/pokemons", pokemonRouter);
 // app.use('/users', usersRouter);
 
 //catch when when request match no route
 app.use((req, res, next) => {
-  const exception = new Error(`Path not found`);
-  exception.statusCode = 404;
-  next(exception);
+	const exception = new Error(`Path not found`);
+	exception.statusCode = 404;
+	next(exception);
 });
 
 //customize express error handling middleware
 app.use((err, req, res, next) => {
-  res.status(err.statusCode).send(err.message);
+	res.status(err.statusCode).send(err.message);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).send("Wellcome to Pokemon World");
+	res.status(200).send("Wellcome to Pokemon World");
 });
-
-const pokemonRouter = require("./pokemon-api.js");
-router.use("/pokemons", pokemonRouter);
 
 module.exports = router;
